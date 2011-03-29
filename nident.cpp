59c114b65c02232e424e2d8024bbd92d7b8d614e
Nicholas J. Kain <nicholas@kain.us>
2011-03-29 00:18:22 -0400

    Switch to using boost program_options.  Support chrooting.

--- nident.cpp
+++ nident.cpp
@@ -1,5 +1,5 @@
 /* nident.c - ident server
- * Time-stamp: <2011-03-27 19:44:09 nk>
+ * Time-stamp: <2011-03-29 00:16:51 nk>
  *
  * (c) 2004-2011 Nicholas J. Kain <njkain at gmail dot com>
  * All rights reserved.
@@ -52,6 +52,7 @@
 #include <getopt.h>
 
 #include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 
 #include "identclient.hpp"
 
@@ -67,8 +68,9 @@ extern "C" {
 #include "strlist.h"
 }
 
-boost::asio::io_service io_service;
+namespace po = boost::program_options;
 
+boost::asio::io_service io_service;
 bool gParanoid = false;
 
 static void sighandler(int sig)
@@ -99,162 +101,127 @@ static void fix_signals(void) {
     sigaction(SIGTERM, &sa, NULL);
 }
 
-int main(int argc, char** argv) {
-    int c, t, uid = 0, gid = 0, len;
-    unsigned int port = 0;
-    std::string pidfile;
-    char *p;
-    struct passwd *pws;
-    struct group *grp;
+int main(int ac, char *av[]) {
+    int uid = 0, gid = 0;
+    std::string pidfile, chroot_path;
     std::vector<ClientListener *> listeners;
     std::vector<std::string> addrlist;
 
     gflags_log_name = const_cast<char *>("nident");
 
-    while (1) {
-	int option_index = 0;
-	static struct option long_options[] = {
-	    {"detach", 0, 0, 'd'},
-	    {"nodetach", 0, 0, 'n'},
-	    {"pidfile", 1, 0, 'f'},
-	    {"quiet", 0, 0, 'q'},
-	    {"max-bytes", 1, 0, 'B'},
-	    {"address", 1, 0, 'a'},
-	    {"port", 1, 0, 'p'},
-	    {"user", 1, 0, 'u'},
-	    {"group", 1, 0, 'g'},
-	    {"paranoid", 0, 0, 'P'},
-	    {"help", 0, 0, 'h'},
-	    {"version", 0, 0, 'v'},
-	    {0, 0, 0, 0}
-	};
+    po::options_description desc("Options");
+    desc.add_options()
+	("paranoid,p",
+	 "return UNKNOWN-ERROR for all errors except INVALID-PORT (prevents inference of used ports)")
+        ("detach,d", "run as a background daemon (default)")
+        ("nodetach,n", "stay attached to TTY")
+        ("quiet,q", "don't print to std(out|err) or log")
+        ("pidfile,f", po::value<std::string>(),
+         "path to process id file")
+        ("chroot,c", po::value<std::string>(),
+         "path in which nident should chroot itself")
+	("max-bytes,b", po::value<int>(),
+	 "maximum number of bytes allowed from a client")
+	("address,a", po::value<std::vector<std::string> >(),
+	 "'address[:port]' on which to listen (default all local)")
+	("user,u", po::value<std::string>(),
+	 "user name that nident should run as")
+	("group,g", po::value<std::string>(),
+	 "group name that nident should run as")
+        ("help,h", "print help message")
+        ("version,v", "print version information")
+        ;
+    po::positional_options_description p;
+    p.add("address", -1);
+    po::variables_map vm;
+    try {
+        po::store(po::command_line_parser(ac, av).
+                  options(desc).positional(p).run(), vm);
+    } catch(std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    po::notify(vm);
 
-	c = getopt_long(argc, argv, "dnf:qB:a:p:ou:g:Phv",
-			long_options, &option_index);
-	if (c == -1)
-	    break;
-
-	switch (c) {
-
-	    case 'h':
-		printf("nident %s, ident server.\n", NIDENT_VERSION);
-		printf(
-		    "Copyright (c) 2010-2011 Nicholas J. Kain\n"
-		    "Usage: nident [OPTIONS]\n"
-		    "  -d, --detach                detach from TTY and daemonize (default)\n"
-		    "  -n, --nodetach              stay attached to TTY\n"
-		    "  -q, --quiet                 don't print to std(out|err) or log\n");
-		printf(
-		    "  -B, --max-bytes             maximum number of bytes allowed from a client\n"
-		    "  -a, --address               address on which to listen (default all local)\n"
-		    "  -p, --port                  port on which to listen (only one allowed)\n"
-		    "  -f, --pidfile               pidfile path\n");
-		printf(
-		    "  -u, --user                  user name that nident should run as\n"
-		    "  -g, --group                 group name that nident should run as\n"
-		    "  -P, --paranoid              return UNKNOWN-ERROR for all errors except\n"
-                    "                              INVALID-PORT (prevents inference of used ports)\n"
-		    "  -h, --help                  print this help and exit\n"
-		    "  -v, --version               print license information and exit\n");
-		exit(EXIT_FAILURE);
-		break;
-
-	    case 'v':
-		printf("nident %s, ident server.\n", NIDENT_VERSION);
-		printf(
-		    "Copyright (c) 2010-2011 Nicholas J. Kain\n"
-		    "All rights reserved.\n\n"
-		    "Redistribution and use in source and binary forms, with or without\n"
-		    "modification, are permitted provided that the following conditions are met:\n\n"
-		    "- Redistributions of source code must retain the above copyright notice,\n"
-		    "  this list of conditions and the following disclaimer.\n"
-		    "- Redistributions in binary form must reproduce the above copyright notice,\n"
-		    "  this list of conditions and the following disclaimer in the documentation\n"
-		    "  and/or other materials provided with the distribution.\n\n"
-		    "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n"
-		    "AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n"
-		    "IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE\n"
-		    "ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE\n"
-		    "LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR\n"
-		    "CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF\n"
-		    "SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS\n"
-		    "INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN\n"
-		    "CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)\n"
-		    "ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE\n"
-		    "POSSIBILITY OF SUCH DAMAGE.\n");
-		exit(EXIT_FAILURE);
-		break;
-
-	    case 'd':
-		gflags_detach = 1;
-		break;
-
-	    case 'n':
-		gflags_detach = 0;
-		break;
-
-	    case 'q':
-		gflags_quiet = 1;
-		break;
-
-	    case 'B':
-		max_client_bytes = atoi(optarg);
-		if (max_client_bytes < 64)
-		    max_client_bytes = 64;
-		else if (max_client_bytes > 1024)
-		    max_client_bytes = 1024;
-		break;
-
-	    case 'p':
-		port = atoi(optarg);
-		break;
-
-	    case 'f':
-		len = strlen(optarg);
-		pidfile = std::string(optarg, len);
-		break;
-
-	    case 'a':
-		len = strlen(optarg);
-		addrlist.push_back(std::string(optarg, len));
-		break;
-
-	    case 'u':
-		t = (unsigned int) strtol(optarg, &p, 10);
-		if (*p != '\0') {
-		    pws = getpwnam(optarg);
-		    if (pws) {
-			uid = (int)pws->pw_uid;
-			if (!gid)
-			    gid = (int)pws->pw_gid;
-		    } else suicide("invalid uid specified");
-		} else
-		    uid = t;
-		break;
-
-	    case 'g':
-		t = (unsigned int) strtol(optarg, &p, 10);
-		if (*p != '\0') {
-		    grp = getgrnam(optarg);
-		    if (grp) {
-			gid = (int)grp->gr_gid;
-		    } else suicide("invalid gid specified");
-		} else
-		    gid = t;
-		break;
-
-	    case 'P':
-		gParanoid = true;
-		break;
+    if (vm.count("help")) {
+        std::cout << "nident " << NIDENT_VERSION << ", ident server.\n"
+		  << "Copyright (c) 2010-2011 Nicholas J. Kain\n"
+		  << av[0] << " [options] addresses...\n"
+		  << desc << std::endl;
+        return 1;
+    }
+    if (vm.count("version")) {
+	std::cout << "nident " << NIDENT_VERSION << ", ident server.\n" <<
+	    "Copyright (c) 2010-2011 Nicholas J. Kain\n"
+	    "All rights reserved.\n\n"
+	    "Redistribution and use in source and binary forms, with or without\n"
+	    "modification, are permitted provided that the following conditions are met:\n\n"
+	    "- Redistributions of source code must retain the above copyright notice,\n"
+	    "  this list of conditions and the following disclaimer.\n"
+	    "- Redistributions in binary form must reproduce the above copyright notice,\n"
+	    "  this list of conditions and the following disclaimer in the documentation\n"
+	    "  and/or other materials provided with the distribution.\n\n"
+	    "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n"
+	    "AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n"
+	    "IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE\n"
+	    "ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE\n"
+	    "LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR\n"
+	    "CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF\n"
+	    "SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS\n"
+	    "INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN\n"
+	    "CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)\n"
+	    "ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE\n"
+	    "POSSIBILITY OF SUCH DAMAGE.\n";
+        return 1;
+    }
+    if (vm.count("paranoid"))
+	gParanoid = true;
+    if (vm.count("detach"))
+	gflags_detach = 1;
+    if (vm.count("nodetach"))
+	gflags_detach = 0;
+    if (vm.count("quiet"))
+	gflags_quiet = 1;
+    if (vm.count("max-bytes")) {
+	max_client_bytes = vm["max-bytes"].as<int>();
+	if (max_client_bytes < 64)
+	    max_client_bytes = 64;
+	else if (max_client_bytes > 1024)
+	    max_client_bytes = 1024;
+    }
+    if (vm.count("pidfile"))
+	pidfile = vm["pidfile"].as<std::string>();
+    if (vm.count("chroot"))
+	chroot_path = vm["chroot"].as<std::string>();
+    if (vm.count("address"))
+	addrlist = vm["address"].as<std::vector<std::string> >();
+    if (vm.count("user")) {
+	auto t = vm["user"].as<std::string>();
+	try {
+	    uid = boost::lexical_cast<unsigned int>(t);
+	} catch (boost::bad_lexical_cast &) {
+	    auto pws = getpwnam(t.c_str());
+	    if (pws) {
+		uid = (int)pws->pw_uid;
+		if (!gid)
+		    gid = (int)pws->pw_gid;
+	    } else suicide("invalid uid specified");
+	}
+    }
+    if (vm.count("group")) {
+	auto t = vm["group"].as<std::string>();
+	try {
+	    gid = boost::lexical_cast<unsigned int>(t);
+	} catch (boost::bad_lexical_cast &) {
+	    auto grp = getgrnam(t.c_str());
+	    if (grp) {
+		gid = (int)grp->gr_gid;
+	    } else suicide("invalid gid specified");
 	}
     }
 
     if (gflags_detach)
 	if (daemon(0,0))
 	    suicide("detaching fork failed");
-
-    if (!port)
-	suicide("no listening port specified!");
 
     if (pidfile.size() && file_exists(pidfile.c_str(), "w"))
 	write_pid(pidfile.c_str());
@@ -264,20 +231,48 @@ int main(int argc, char** argv) {
     ncm_fix_env(uid, 0);
 
     if (!addrlist.size()) {
-	auto ep = boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v6(), port);
+	auto ep = boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v6(), 113);
 	auto cl = new ClientListener(ep);
 	listeners.push_back(cl);
     } else
 	for (auto i = addrlist.cbegin(); i != addrlist.cend(); ++i) {
-	    auto addy = boost::asio::ip::address::from_string(*i);
-	    auto ep = boost::asio::ip::tcp::endpoint(addy, port);
-	    auto cl = new ClientListener(ep);
-	    listeners.push_back(cl);
+	    std::string addr = *i;
+	    int port = 113;
+	    auto loc = addr.find(":");
+	    if (loc != std::string::npos) {
+		auto pstr = addr.substr(loc + 1);
+		try {
+		    port = boost::lexical_cast<unsigned short>(pstr);
+		} catch (boost::bad_lexical_cast &) {
+		    std::cout << "bad port in address '" << addr
+			      << "', defaulting to 113" << std::endl;
+		}
+		addr.erase(loc);
+	    }
+	    std::cout << "addr " << addr << ":" << port << std::endl;
+	    try {
+		auto addy = boost::asio::ip::address::from_string(addr);
+		auto ep = boost::asio::ip::tcp::endpoint(addy, port);
+		auto cl = new ClientListener(ep);
+		listeners.push_back(cl);
+	    } catch (boost::system::error_code &ec) {
+		std::cout << "bad address: " << addr << std::endl;
+	    }
 	}
     addrlist.clear();
 
-    if (uid != 0 || gid != 0)
+    if (chroot_path.size()) {
+	if (getuid())
+	    suicide("root required for chroot\n");
+	if (chdir(chroot_path.c_str()))
+	    suicide("failed to chdir(%s)\n", chroot_path.c_str());
+	if (chroot(chroot_path.c_str()))
+	    suicide("failed to chroot(%s)\n", chroot_path.c_str());
+	chroot.clear();
+    }
+    if (uid != 0 || gid != 0) {
 	drop_root(uid, gid);
+    }
 
     /* Cover our tracks... */
     pidfile.clear();
